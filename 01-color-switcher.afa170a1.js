f6f95b9dc2a876f1e61d6983cac3e5b3f6fe8552
Oleksii-Kryventsov <114357372+Oleksii-Kryventsov@users.noreply.github.com>
2023-03-21 00:16:30 +0000

    Deploying to gh-pages from @ Oleksii-Kryventsov/goit-js-hw-09@9ca820cd7a87dc65dd1e924809b5785cc9dbad17 🚀

---
+++ 01-color-switcher.afa170a1.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");let o=null;t.addEventListener("click",(function(){t.disabled=!0,o=setInterval((()=>{let t=`#${Math.floor(16777215*Math.random()).toString(16)}`;n.style.background=t}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,clearInterval(o)}));
+//# sourceMappingURL=01-color-switcher.afa170a1.js.map
---
+++ 01-color-switcher.f9a36d74.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body"),o=null;t.addEventListener("click",(function(){t.disabled=!0,o=setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16));n.style.background=t}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,clearInterval(o)}))}();
+//# sourceMappingURL=01-color-switcher.f9a36d74.js.map
